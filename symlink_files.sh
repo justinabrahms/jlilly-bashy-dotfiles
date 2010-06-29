1b7f57ee97fe577d5afe625f2654ba12c469e978
Justin Lilly <justin@justinlilly.com>
2010-06-29 17:58:13 -0400

    Adding some new considerations to the symlink_files scripts

--- symlink_files.sh
+++ symlink_files.sh
@@ -3,6 +3,12 @@
 shopt -s dotglob # shows dots in file globbing
 
 for i in *; do
-ln -s `pwd`/$i $HOME/$i
+  rm $HOME/$i 2>/dev/null # remove duplicates
+  ln -s `pwd`/$i $HOME/$i # copy new files in
 done
+
+# Clean up after our naieve loop
 rm $HOME/symlink_files.sh
+rm -rf $HOME/.git
+
+echo "Symlinking successful."
